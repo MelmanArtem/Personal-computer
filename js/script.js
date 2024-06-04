0e3a98da15c06cf3895cefff3c6c76006694eab2
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-06-05 02:03:45 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
+const contents = document.querySelectorAll('.program-line__content');
 
+contents.forEach((elem) => {
+	const title = elem.querySelector('.program-line__title')
 
+	title.onclick = () => {
+			console.log(title);
+	}
+
+	console.dir(title);
+});
