d1b06633f4dd087a903b9b8c8285f67d26d99e57
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-06-06 00:16:36 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 const contents = document.querySelectorAll('.program-line__content');
 
 contents.forEach((elem) => {
-	const title = elem.querySelector('.program-line__title')
+	const title = elem.querySelector('.program-line__title');
+	const descr = elem.querySelector('.program-line__descr');
 
-	title.onclick = () => {
-			console.log(title);
-	}
-
-	console.dir(title);
+	title.addEventListener('click', () => {
+		descr.classList.toggle('active')
+	});
 });
