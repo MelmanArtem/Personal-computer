6c8bcd32877e82b6f13e1eca74aab9128557a9c8
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-06-14 16:32:00 +0300

    add timer

--- js/timer.js
+++ js/timer.js
@@ -3,12 +3,23 @@ const hoursBlock = document.querySelector('.timer__hours');
 const minutesBlock = document.querySelector('.timer__minutes');
 const secondsBlock = document.querySelector('.timer__seconds');
 
-let interval 
+let interval
+
+const numWord = (value, words) => {
+	value = Math.abs(value) % 100
+	const lastNum = value % 10
+
+	if (value > 10 & value < 20) return words[2]
+	if (lastNum > 1 && lastNum < 5) return words[1]
+	if (lastNum === 1) return words[0]
+	return words[2]
+}
+
 
 const updateTimer = () => {
 	const date = new Date();
 	const dateDeadline = new Date('25 june 2024').getTime();
-	const timeRemaining = (dateDeadline - date) /1000;
+	const timeRemaining = (dateDeadline - date) / 1000;
 
 	const days = Math.floor((timeRemaining / 60 / 60 / 24));
 	const hours = Math.floor((timeRemaining / 60 / 60) % 24);
@@ -24,6 +35,11 @@ const updateTimer = () => {
 	hoursBlock.textContent = fHours
 	minutesBlock.textContent = fMinutes
 	secondsBlock.textContent = fSeconds
+
+	secondsBlock.nextElementSibling.textContent = numWord(seconds, ['Секунда', 'Секунды', 'Секунд'])
+	minutesBlock.nextElementSibling.textContent = numWord(minutes, ['Минута', 'Минуты', 'Минут'])
+	hoursBlock.nextElementSibling.textContent = numWord(hours, ['Час', 'Часа', 'Часов'])
+	daysBlock.nextElementSibling.textContent = numWord(hours, ['День', 'Дня', 'Дней'])
 
 	if (timeRemaining <= 0) {
 		clearInterval(interval)
