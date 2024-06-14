d63cdc36be70c9c38aed612daa1ced67bba36450
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-06-14 14:25:07 +0300

    add changes

--- js/timer.js
+++ js/timer.js
@@ -1,24 +1,38 @@
+const daysBlock = document.querySelector('.timer__days')
 const hoursBlock = document.querySelector('.timer__hours');
 const minutesBlock = document.querySelector('.timer__minutes');
 const secondsBlock = document.querySelector('.timer__seconds');
 
+let interval 
 
 const updateTimer = () => {
 	const date = new Date();
+	const dateDeadline = new Date('25 june 2024').getTime();
+	const timeRemaining = (dateDeadline - date) /1000;
 
-	const hours = date.getHours();
-	const minutes = date.getMinutes();
-	const seconds = date.getSeconds();
+	const days = Math.floor((timeRemaining / 60 / 60 / 24));
+	const hours = Math.floor((timeRemaining / 60 / 60) % 24);
+	const minutes = Math.floor((timeRemaining / 60) % 60);
+	const seconds = Math.floor(timeRemaining % 60);
 
+	const fDays = days < 10 ? '0' + days : days
 	const fHours = hours < 10 ? '0' + hours : hours
 	const fMinutes = minutes < 10 ? '0' + minutes : minutes
 	const fSeconds = seconds < 10 ? '0' + seconds : seconds
 
-
-
+	daysBlock.textContent = fDays
 	hoursBlock.textContent = fHours
 	minutesBlock.textContent = fMinutes
 	secondsBlock.textContent = fSeconds
+
+	if (timeRemaining <= 0) {
+		clearInterval(interval)
+		daysBlock.textContent = '00'
+		hoursBlock.textContent = '00'
+		minutesBlock.textContent = '00'
+		secondsBlock.textContent = '00'
+	}
 };
 
-setInterval(updateTimer, 500)
+updateTimer();
+interval = setInterval(updateTimer, 500)
