91bf186632d53d23a633e6278bcc6cce7bdfeb5e
MelmanArtem <122752598+MelmanArtem@users.noreply.github.com>
2024-06-17 00:06:55 +0300

    add sendform

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,10 +1,23 @@
-const contents = document.querySelectorAll('.program-line__content');
+const accordeon = () => {
+	const contents = document.querySelectorAll('.program-line__content');
+	const contentDescr = document.querySelectorAll('.program-line__descr');
 
-contents.forEach((elem) => {
-	const title = elem.querySelector('.program-line__title');
-	const descr = elem.querySelector('.program-line__descr');
+	contents.forEach((elem) => {
+		const title = elem.querySelector('.program-line__title');
+		const descr = elem.querySelector('.program-line__descr');
 
-	title.addEventListener('click', () => {
-		descr.classList.toggle('active')
+		descr.style.transition = 'height .4s'
+		descr.style.overflow = 'hidden'
+
+		title.addEventListener('click', () => {
+			contentDescr.forEach((tab) => {
+				if (tab === descr) {
+					tab.style.height = tab.scrollHeight + 'px'
+				} else {
+					tab.style.height = 0
+				}
+			});
+		});
 	});
-});
+}
+ accordeon()
